5ef7c45046deb70aa3e850cb4834084dbb89f531
Ulysses Fredericci <ulysses@linuxcit016987.cit>
2017-08-21 22:49:38 -0300

    Added ssh quiet mode

--- hadoop/docker/entrypoint.sh
+++ hadoop/docker/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+rm -rf ~/.ssh
+mkdir ~/.ssh
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+service ssh restart
+
 /usr/local/hadoop/sbin/start-dfs.sh
 /usr/local/hadoop/sbin/start-yarn.sh
 hdfs namenode -format
